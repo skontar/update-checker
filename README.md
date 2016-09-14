fd5d7876c91abcd9cdc2fa7d90a739bf0d72228c
Stanislav Kontar <skontar@redhat.com>
2016-09-14 13:21:53 +0200

    Minor fixes, added refresh to dnf command

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Fedora XFCE Update Checker
 
 Update Checker is a simple application used to check for updates on Fedora 22+ systems (e.g. the
-ones using DNF as package manager). It is meant for XFCE desktop environment but should be working 
-in any desktop environment supporting notifications and system tray notification icons with slight 
+ones using DNF as package manager). It is meant for XFCE desktop environment but should be working
+in any desktop environment supporting notifications and system tray notification icons with slight
 modification.
 
 It checks for updates in time intervals and notifies user about updates being ready by notification
@@ -12,18 +12,18 @@ updates and checks every 4 hours.
 
 ## Installation
 
-Prerequisities are `python3` and `python3-gobject` packages, which should be installed on Fedora by 
+Prerequisites are `python3` and `python3-gobject` packages, which should be installed on Fedora by
 default.
 
-Download the application directory and put it anywhere. Add `update_checker.py` to your startup 
+Download the application directory and put it anywhere. Add `update_checker.py` to your startup
 script.
 
 
 ## Usage
 
-Run `update_checker.py`. The application runs on background and appears only when updates are 
-detected. It also has a few commanline switches:
-    
+Run `update_checker.py`. The application runs on background and appears only when updates are
+detected. It also has a few commandline switches:
+
     -h, --help            show this help message and exit
     -a, --all             report on all updates, not just security ones
                           (default: False)
@@ -33,8 +33,8 @@ detected. It also has a few commanline switches:
     -i INTERVAL, --interval INTERVAL
                           check for updates every INTERVAL hours (can be
                           fraction) (default: 4.0)
-    
-    
+
+
 ## Testing
 
 It was tested on various Fedora 22 and Fedora 23 desktop systems, mostly XFCE spins.
@@ -42,7 +42,7 @@ It was tested on various Fedora 22 and Fedora 23 desktop systems, mostly XFCE sp
 
 ## Icons artwork
 
-Website: [IconArchive](http://www.iconarchive.com/show/soft-scraps-icons-by-hopstarter.html)    
+Website: [IconArchive](http://www.iconarchive.com/show/soft-scraps-icons-by-hopstarter.html)  
 Artist: [Hopstarter (Jojo Mendoza)](http://www.iconarchive.com/artist/hopstarter.html)  
-License: [CC Attribution-Noncommercial-No Derivate 4.0](http://creativecommons.org/licenses/by-nc-nd/4.0/)  
+License: [CC Attribution-Noncommercial-No Derivate 4.0](http://creativecommons.org/licenses/by-nc-nd/4.0/)
 
--- update_checker.py
+++ update_checker.py
@@ -23,14 +23,14 @@ from threading import Thread
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Notify', '0.7')
-from gi.repository import GLib, Gtk, Notify  # @IgnorePep8
+from gi.repository import GLib, Gtk, Notify
 
 
-COMMAND_DNF_UPDATEINFO = 'nice -n 19 dnf updateinfo list updates'
+COMMAND_DNF_UPDATEINFO = 'nice -n 19 dnf updateinfo list updates --refresh'
 COMMAND_DNF_UPGRADE = 'sudo dnf upgrade'
 BASH_COMMAND = 'bash -c \'echo {dnf}; {dnf}; read -p "Press ENTER to close window"\''
 COMMAND_TERMINAL = 'xfce4-terminal --maximize -x {}'.format(BASH_COMMAND)
-COMMAND_TERMINAL_DROPDOWN = 'xfce4-terminal --tab --title Update --drop-down -x {}'.format(BASH_COMMAND)
+COMMAND_DROPDOWN = 'xfce4-terminal --tab --title Update --drop-down -x {}'.format(BASH_COMMAND)
 
 WD = path.dirname(path.abspath(sys.argv[0]))  # Manage to run script anywhere in the path
 ICON_YELLOW = path.join(WD, 'Hopstarter-Soft-Scraps-Button-Blank-Yellow.ico')
@@ -68,7 +68,7 @@ def dnf_upgrade(normal_terminal=False, packages=None):
     if normal_terminal:
         command = COMMAND_TERMINAL
     else:
-        command = COMMAND_TERMINAL_DROPDOWN
+        command = COMMAND_DROPDOWN
     if packages is None:
         call(command.format(dnf=COMMAND_DNF_UPGRADE), shell=True)
     else:
@@ -91,21 +91,7 @@ class Application:
         self.status_icon = Gtk.StatusIcon(visible=False)
 
         # Menu
-        self.menu = Gtk.Menu()
-
-        upgrade_security = Gtk.ImageMenuItem('Upgrade security')
-        upgrade_security.set_image(Gtk.Image.new_from_icon_name(Gtk.STOCK_STOP, Gtk.IconSize.MENU))
-        upgrade_security.connect('activate', self.on_menu_upgrade_security, None)
-        self.menu.append(upgrade_security)
-
-        upgrade = Gtk.ImageMenuItem('Upgrade all')
-        upgrade.set_image(Gtk.Image.new_from_icon_name(Gtk.STOCK_APPLY, Gtk.IconSize.MENU))
-        upgrade.connect('activate', self.on_menu_upgrade, None)
-        self.menu.append(upgrade)
-
-        close = Gtk.ImageMenuItem(Gtk.STOCK_QUIT, use_stock=True)
-        close.connect('activate', self.on_menu_close, None)
-        self.menu.append(close)
+        self.menu = None  # Will be built dynamically
 
         # Callbacks
         self.status_icon.connect('activate', self.on_icon_click)
@@ -129,7 +115,7 @@ class Application:
 
     def worker_upgrade(self, all_packages=False):
         """
-        Runs DNF update based on user preferences in another thread to not block GUI.
+        Runs DNF upgrade based on user preferences in another thread to not block GUI.
         """
         if all_packages:
             dnf_upgrade(self.normal_terminal)
@@ -138,28 +124,54 @@ class Application:
 
     def found_updates(self):
         """
-        Shows appropriate status icon, tooltip and notification.
+        Shows appropriate status icon, tooltip, notification, and menu.
         """
         if self.security_updates_nr > 0:
-            template = 'Found {} updates, {} of them are security related'
-            message = template.format(self.updates_nr, self.security_updates_nr)
+            template = '{} security updates found out of {} total updates'
+            message = template.format(self.security_updates_nr, self.updates_nr)
             self.status_icon.set_from_file(ICON_RED)
         else:
-            template = 'Found {} updates'
+            template = '{} updates found'
             message = template.format(self.updates_nr)
             self.status_icon.set_from_file(ICON_YELLOW)
+
         self.status_icon.set_tooltip_text(message)
+        self.build_menu()
         self.status_icon.set_visible(True)
+
         self.notification = Notify.Notification.new(message)
         if self.security_updates_nr > 0:
-            self.notification.add_action('clicked_upgrade_security', 'Upgrade security',
+            self.notification.add_action('clicked_upgrade_security',
+                                         'Upgrade security ({})'.format(self.security_updates_nr),
                                          self.on_notification_upgrade_security)
-        self.notification.add_action('clicked_upgrade', 'Upgrade all', self.on_notification_upgrade)
+        self.notification.add_action('clicked_upgrade',
+                                     'Upgrade all ({})'.format(self.updates_nr),
+                                     self.on_notification_upgrade)
         self.notification.add_action('clicked_dismiss', 'Dismiss', self.on_notification_dismiss)
         self.notification.show()
 
     def no_updates(self):
         self.status_icon.set_visible(False)
+
+    def build_menu(self):
+        self.menu = Gtk.Menu()
+
+        if self.security_updates_nr > 0:
+            upgrade_security = Gtk.ImageMenuItem('Upgrade security ({})'
+                                                 .format(self.security_updates_nr))
+            upgrade_security.set_image(Gtk.Image.new_from_icon_name(Gtk.STOCK_STOP,
+                                                                    Gtk.IconSize.MENU))
+            upgrade_security.connect('activate', self.on_menu_upgrade_security, None)
+            self.menu.append(upgrade_security)
+
+        upgrade = Gtk.ImageMenuItem('Upgrade all ({})'.format(self.updates_nr))
+        upgrade.set_image(Gtk.Image.new_from_icon_name(Gtk.STOCK_APPLY, Gtk.IconSize.MENU))
+        upgrade.connect('activate', self.on_menu_upgrade, None)
+        self.menu.append(upgrade)
+
+        close = Gtk.ImageMenuItem(Gtk.STOCK_QUIT, use_stock=True)
+        close.connect('activate', self.on_menu_close, None)
+        self.menu.append(close)
 
     def upgrade(self, all_packages=False):
         self.status_icon.set_visible(False)
